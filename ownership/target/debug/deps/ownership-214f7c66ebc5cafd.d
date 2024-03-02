c023a04a704e0bb7d5a845341baec129f70bfe96
Faiz Mohammed <Faizmohd11251@gmail.com>
2024-03-02 20:47:13 +0530

    Initial commit, Added: notes for rust

---
+++ ownership/target/debug/deps/ownership-214f7c66ebc5cafd.d
@@ -0,0 +1,5 @@
+C:\Users\Muskaan\Desktop\Learn Rust\ownership\target\debug\deps\libownership-214f7c66ebc5cafd.rmeta: src\main.rs
+
+C:\Users\Muskaan\Desktop\Learn Rust\ownership\target\debug\deps\ownership-214f7c66ebc5cafd.d: src\main.rs
+
+src\main.rs:
